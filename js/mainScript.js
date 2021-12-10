0210be698d12948d1896c744932bd5ba61351068
ayman-alshantaf <ayman1998.alshantaf@gmail.com>
2021-12-10 22:18:01 +0200

    new update

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,48 +1,60 @@
-$('.type-card').slick({
-    infinite: false,
-    speed: 400,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay:true,
-    autoplaySpeed: 2000,
-    prevArrow: "<i class=\"fa fa-chevron-left\" aria-hidden=\"true\"></i>\n",
-    nextArrow: "<i class=\"fa fa-chevron-right\" aria-hidden=\"true\"></i>\n",
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
+$(function() {
+    $('.type-card').slick({
+        infinite: true,
+        speed: 600,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay:true,
+        autoplaySpeed: 1500,
+        prevArrow: "<i class=\"fa fa-chevron-left\" aria-hidden=\"true\"></i>\n",
+        nextArrow: "<i class=\"fa fa-chevron-right\" aria-hidden=\"true\"></i>\n",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 720,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
-        },
-        {
-            breakpoint: 720,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
+        ]
+    });
+
+    $(".category span").on('click', function(){
+        var filter = $(this).data('category');
+        $(".type-card").slick('slickUnfilter');
+
+        if(filter == 'tilbud'){
+            $(".type-card").slick('slickFilter','.tilbud');
         }
-    ]
+        else if(filter == 'nyheder'){
+            $(".type-card").slick('slickFilter','.nyheder');
+        }
+        else if(filter == 'topsalgere'){
+            $(".type-card").slick('slickFilter','.topsalgere');
+        }
+
+    })
 });
+
 $('.category span').on("click" , function (){
     $(this).siblings().removeClass("active");
     $(this).addClass("active");
 });
-$('.category span').on("click" , function () {
-    var data_category = $(this).data('category');
-    data_category='#' + data_category;
-    console.log(data_category)
 
-    $(data_category).show();
-    $(data_category).siblings().hide();
-});
 
