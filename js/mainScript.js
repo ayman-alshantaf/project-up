475e93044a58dd57305790a9ff4d2daf0a90d2c6
ayman-alshantaf <ayman1998.alshantaf@gmail.com>
2021-12-12 19:33:44 +0200

    update search and modal and zoom

--- js/mainScript.js
+++ js/mainScript.js
@@ -4,7 +4,7 @@ $(function() {
         speed: 600,
         slidesToShow: 4,
         slidesToScroll: 4,
-        autoplay:true,
+        autoplay: true,
         autoplaySpeed: 1500,
         prevArrow: "<i class=\"fa fa-chevron-left\" aria-hidden=\"true\"></i>\n",
         nextArrow: "<i class=\"fa fa-chevron-right\" aria-hidden=\"true\"></i>\n",
@@ -35,26 +35,59 @@ $(function() {
         ]
     });
 
-    $(".category span").on('click', function(){
+    $(".category span").on('click', function () {
         var filter = $(this).data('category');
         $(".type-card").slick('slickUnfilter');
 
-        if(filter == 'tilbud'){
-            $(".type-card").slick('slickFilter','.tilbud');
-        }
-        else if(filter == 'nyheder'){
-            $(".type-card").slick('slickFilter','.nyheder');
-        }
-        else if(filter == 'topsalgere'){
-            $(".type-card").slick('slickFilter','.topsalgere');
+        if (filter == 'tilbud') {
+            $(".type-card").slick('slickFilter', '.tilbud');
+        } else if (filter == 'nyheder') {
+            $(".type-card").slick('slickFilter', '.nyheder');
+        } else if (filter == 'topsalgere') {
+            $(".type-card").slick('slickFilter', '.topsalgere');
         }
 
-    })
+    });
+    $('.category span').on("click", function () {
+        $(this).siblings().removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".zoom").ezPlus(
+        {
+            tint: true,
+            tintColour: '#b1b0b0',
+            tintOpacity: 0.5,
+            zoomWindowFadeIn: 500,
+            zoomWindowFadeOut: 500,
+            lensFadeIn: 500,
+            lensFadeOut: 500,
+            easing: true,
+            zoomWindowWidth: 250,
+            zoomWindowHeight: 280,
+            borderSize: 1,
+            borderColour: '#888',
+            lensBorderColour: '#37e80b',
+            zoomLevel: 1.2,
+            zoomTintFadeIn: true,
+            zoomTintFadeOut: true,
+            zoomWindowOffsetX: 25,
+
+        }
+    );
+
+
+    $(window).on('resize', function () {
+        var win = $(this); //this = window
+        if (win.width() <= 850) {
+            $('.ZoomContainer').remove();
+        }
+    });
+
 });
 
-$('.category span').on("click" , function (){
-    $(this).siblings().removeClass("active");
-    $(this).addClass("active");
-});
+
+
+
 
 
